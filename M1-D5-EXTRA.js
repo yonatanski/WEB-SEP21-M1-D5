57afe9706a268893132b06a5fc43c6a1dc8c48e6
yonatanski <88892819+yonatanski@users.noreply.github.com>
2021-10-04 18:36:52 +0200

    completed

---
+++ M1-D5-EXTRA.js
@@ -0,0 +1,82 @@
+// Additional assignments for Day 5
+
+/* EXTRA 1
+ Write a function called "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5 or not.
+ The function should return the sum of just the numbers bigger than 5.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 2
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "shoppingCartTotal" which calculates the total due to the shop.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 3
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 4
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in it.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 5
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 6
+ Create a function called "loopUntil" which receives an integer x between 0 and 9 as a parameter.
+ The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 7
+ Write a function called "average" which receives an array and returns the average numerical value. The function automatically skips non-numeric entries in the array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 8
+ Write a function called "longest" to find the longest string from a given array of strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 9
+ Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
+ The function should return true if the emailContent string does not contain the words SPAM or SCAM.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 10
+ Write a function that receives a date as a parameter and calculates the number of days passed since the given date.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 11
+ Write a function called "matrixGenerator" which receives two integers, x and y, as parameters.
+ The result should be a matrix of x times y with, as value, the index of the position.
+ Ex.: x = 3, y = 2
+ ["00","01","02"
+ "10","11","12"]
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* WHEN YOU ARE FINISHED
+ Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
+*/
--- M1-D5.js
+++ M1-D5.js
@@ -9,19 +9,21 @@ ASSIGNMENT RULES
 /* EXERCISE 1
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
-const area = function (l1, l2) {
+console.log("----------------->EXERCISE 1<-------------")
+const theArea = function (l1, l2) {
   return l1 * l2;
 }
-area()
+console.log(theArea(11,12))
 //const area = (l1, l2) => l1 * l2;
 
-const areResult = area(3, 4);
-console.log(areResult);
+// const areResult = area(3, 4);
+// console.log(areResult);
 /* EXERCISE 2
-Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should 
+return their sum multiplied by 3.
 */
-
-function crazySum(num1, num2) {
+console.log("\n----------------->EXERCISE 2<-------------")
+const crazySum = function (num1, num2) {
   if (num1 === num2) {
     return (num1 + num2) * 3;
   } else {
@@ -31,11 +33,13 @@ function crazySum(num1, num2) {
 // const crazySum = (num1, num2) =>
 //   num1 === num2 ? (num1 + num2) * 3 : num1 + num2;
 
-console.log(crazySum(3, 3));
+console.log(`When the Number is  equal(===) ${crazySum(3, 3)}`);
+console.log(`When the Number is not equal(!===) ${crazySum(3, 4)}`);
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
 It should return triple their absolute difference if the given number is greater than 19.
 */
+console.log("\n----------------->EXERCISE 3<-------------")
 function crazyDiff(num) {
   if (num > 19) {
     return Math.abs(num - 19) * 3;
@@ -49,7 +53,7 @@ console.log(crazyDiff(3));
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
-
+console.log("\n----------------->EXERCISE 4<-------------")
 function boundary(n) {
   if ((n > 20 && n <= 100) || n === 400) {
     return true;
@@ -64,7 +68,7 @@ console.log(boundary(3));
 Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
-
+console.log("\n----------------->EXERCISE 5<-------------")
 function strivify(str) {
   if (str.startsWith("Strive")) {
     return str;
@@ -79,6 +83,7 @@ console.log(strivify("Strivers"));
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
+console.log("\n----------------->EXERCISE 6<-------------")
 
 function check3and7(n) {
   if (n >= 0 && (n % 3 === 0 || n % 7 === 0)) {
@@ -93,6 +98,7 @@ console.log(check3and7(20));
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
 */
+console.log("\n----------------->EXERCISE 7<-------------")
 
 function reverseString(str) {
   return str.split("").reverse("").join("");
@@ -103,6 +109,7 @@ console.log(reverseString("Strive"));
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
 */
+console.log("\n----------------->EXERCISE 8<-------------")
 
 function upperFirst(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -111,6 +118,7 @@ console.log(upperFirst("strivers"));
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
+console.log("\n----------------->EXERCISE 9<-------------")
 
 function cutString(str) {
   return str.slice(1, str.length - 1);
@@ -120,6 +128,7 @@ console.log(cutString("hello"));
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
+console.log("\n----------------->EXERCISE 10<-------------")
 
 function giveMeRandom(n) {
   const arr = [];
@@ -128,7 +137,8 @@ function giveMeRandom(n) {
   }
   return arr;
 }
-console.log(giveMeRandom(5));
+console.log(giveMeRandom(20),arr.length);
+
 /* WHEN YOU ARE FINISHED
 Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
 */
